6ef1b30b9c6f48a43083052b5dd1864b42bd5e97
Cristina <ecristina.stafie@gmail.com>
2025-04-27 17:24:36 +0100

    testing

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Find the Learn More button
+const learnMoreButton = document.getElementById('learn-more-btn');
+
+// Add a click event
+learnMoreButton.addEventListener('click', function() {
+    // Redirect the user to another website
+    window.location.href = 'https://example.com'; // Change this URL to where you want!
+});
